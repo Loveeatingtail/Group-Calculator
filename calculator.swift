2277df2fb2504ca2cdb1f7b96a15a59ffc0a2767
Ethan Wang <zimralix@outlook.com>
2025-09-11 00:47:38 -0700

    Add public menu code

---
+++ calculator.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+// 显示菜单
+func showMenu() {
+    print("""
+    Select an operation:
+    1: Add
+    2: Subtract
+    3: Multiply
+    4: Divide
+    5: Exponent
+    6: Remainder
+    """)
+}
+
+showMenu() // 显示菜单
