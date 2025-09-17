32f118ce39c55d1597dfc11b504994748ba8ab4f
Steven Zhang <stevenzhang@Stevens-MacBook-Air.local>
2025-09-17 10:27:07 -0700

    cnm

---
+++ calculator.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+// 
+func showMenu() {
+    print("""
+    Select an operation:
+    1: Add
+    2: Subtract
+    3: Multiply
+    4: Divide
+    5: Exponent
+    6: Remainder
+    """)
+}
+
+showMenu() //
+let a = readline()!
+ if a = 1 {add
+} else if a = 2 {sub
+} else if a = 3 {multiply
+} else if a = 4 {divide
+} else if a = 5 {exponent
+} else if a = 6 {remainder
+} else { print("please input an valid number")
