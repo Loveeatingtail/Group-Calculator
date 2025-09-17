8e75885ea2db4b2fa1044cb53c488dbd4ba782f0
Loveeatingtail <loveeatingtail@gmail.com>
2025-09-17 10:33:18 -0700

    Update calculator.swift

--- calculator.swift
+++ calculator.swift
@@ -14,3 +14,11 @@ func showMenu() {
 }
 
 showMenu() // 显示菜单
+let a = readline()!
+ if a = 1 {add
+} else if a = 2 {sub
+} else if a = 3 {multiply
+} else if a = 4 {divide
+} else if a = 5 {exponent
+} else if a = 6 {remainder
+} else { print("please input an valid number")
