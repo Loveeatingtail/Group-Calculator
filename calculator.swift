842ae077a4b59cfca122846657902b917041d289
Steven Zhang <stevenzhang@Stevens-MacBook-Air.local>
2025-09-18 11:38:13 -0700

    everything steven

--- calculator.swift
+++ calculator.swift
@@ -14,11 +14,21 @@ func showMenu() {
 }
 
 showMenu() //
-let a = readline()!
- if a = 1 {add
-} else if a = 2 {sub
-} else if a = 3 {multiply
-} else if a = 4 {divide
-} else if a = 5 {exponent
-} else if a = 6 {remainder
-} else { print("please input an valid number")
+let input = readLine()!
+
+if input == "1" {
+	if let a = readLine()!, let numberA = Int(a),
+	   let b = readLine()!, let numberB = Int(b)
+	let sum = numberA + numberB
+	print(sum)
+} else if input == "2" {
+        if let a = readLine()!, let numberA = Int(a)
+           let b = readLine()!, let numberB = Int(b)
+	let sub = numberA - numberB
+	print(sub)
+}
+/* else if input == 3 {multiply
+} else if input == 4 {divide
+} else if input == 5 {exponent
+} else if input == 6 {remainder
+} else { print("please input an valid number")*/
